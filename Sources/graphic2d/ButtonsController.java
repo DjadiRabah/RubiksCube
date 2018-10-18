ae66c5b39a21762fbd41582b9b30a1165d24fbe0
djadi <djadi@localhost>
2018-10-18 17:44:17 +0200

    Transférer les fichiers vers 'Sources/graphic2d'

---
+++ Sources/graphic2d/ButtonsController.java
@@ -0,0 +1,28 @@
+package graphic2d;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import cube.Cube;
+import shuffle.ShuffleRandom;
+
+public class ButtonsController implements ActionListener
+{
+	protected Cube cube;
+	protected Pattern pattern;
+	
+	public ButtonsController(Cube cube, Pattern pattern)
+	{
+		this.cube = cube;
+		this.pattern = pattern;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) 
+	{
+		if(event.getActionCommand().equals("Melanger"))
+		{
+			this.cube.shuffle(new ShuffleRandom());
+		}
+	}
+}
